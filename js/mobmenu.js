5db3ac163a66e9d75218a1b669e719479707d4cc
vvv7220 <vvv7220@gmail.com>
2019-11-13 13:06:49 +0800

    13.11.2019 13:06

--- js/mobmenu.js
+++ js/mobmenu.js
@@ -1,3 +1,14 @@
-document.querySelector('#menu').addEventListener('click', function(){
-    this.classList.toggle('open_menu');
+let menu = document.querySelector('#menu');
+let menu_btn = document.querySelector('.burger_menu');
+let hamburger = document.querySelector('.hamburger');
+menu_btn.addEventListener('click', function () {
+    menu.classList.toggle('open_menu');
+    if (menu.classList.contains('open_menu')) {
+        hamburger.classList.add('is-active');
+        menu_btn.classList.toggle('moveLeft');
+    }
+    else {
+        hamburger.classList.remove('is-active');
+
+    }
 });
