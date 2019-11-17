1b95543a2ac58c1b8b77d223dfefcb0d4ae314e0
vvv7220 <vvv7220@gmail.com>
2019-11-18 01:28:46 +0800

    18.11.2019

---
+++ js/mymenu.js
@@ -0,0 +1,19 @@
+var m_id = new Array('s_mn_1','s_mn_2','s_mn_3');
+listStart = function allclose() {
+  for (i=0; i < m_id.length; i++){
+  document.getElementById(m_id[i]).style.display = "none";
+  }
+}
+function menuOpen(id){
+  for (i=0; i < m_id.length; i++){
+  if (id != m_id[i]){
+  document.getElementById(m_id[i]).style.display = "none";
+  }
+  }
+  if (document.getElementById(id).style.display == "block"){
+  document.getElementById(id).style.display = "none";
+  }else{
+  document.getElementById(id).style.display = "block";
+  }
+}
+window.onload=listStart;
