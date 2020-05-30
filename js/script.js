a6da20f2146ca533f2eb90d3ad6c0f7dcfbd8402
vvv7220 <vvv7220@gmail.com>
2020-05-30 20:59:57 +0800

    30052020_gallery

--- js/script.js
+++ js/script.js
@@ -28,24 +28,57 @@ document.onscroll = () => {
         }
     }
 
+//Фотогаллерея
+//Скролл
+let page = document.querySelector(".page");
+let gallery = document.querySelector(".gallery");
+let footer = document.querySelector("footer");
+window.addEventListener('scroll', removeHeader);
+function removeHeader(){
+    if(pageYOffset > 80){
+page.classList.add("pagedel");
+footer.classList.add("pagedel");
+gallery.classList.add("showgal");
+    }
+    else{
+        page.classList.remove("pagedel");
+        footer.classList.remove("pagedel");
+        gallery.classList.remove("showgal");
+    }
+    
+}
+//Увеличение картинки
+let picgal = document.querySelectorAll(".picgal");
+let showgal = document.querySelectorAll(".showgal");
+for(let i = 0; i < picgal.length; i++){
+    picgal[i].addEventListener("click", function maxImage(){
+    let imgdiv = document.createElement("div");
+    this.classList.add("resize");
+    imgdiv.className = "wrapimg";
+    imgdiv.prepend(this);
+    gallery.prepend(imgdiv);
+  
+});
+}
+    
 
 
 
-// Mmenu.configs.classNames.selected = "active";
-// Mmenu.configs.offCanvas.page.selector = "#my-page";
-// Mmenu.configs.classNames.vertical = "expand";
-// document.addEventListener(
-//     "DOMContentLoaded", () => {
-//         new Mmenu( "#my-menu", {
-//             hooks: {
-//                 // "openPanel:start": ( panel ) => {
-//                 //     console.log( "Started opening pane: " + panel.id );
-//                 // },
-//                 "openPanel:finish": ( panel ) => {
-//                     $('.hamburger').addClass('is-active');
-//                     console.log( "Finished opening panel: " + panel.id );
-//                 }
-//             }
-//         });
-//     }
-// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
